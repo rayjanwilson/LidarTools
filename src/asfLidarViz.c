6d18e7d68d66fe8f9f03eaf2e35d5ae5c0064a46
Rayjan Wilson <rayjan.wilson@gmail.com>
2010-10-26 16:19:10 -0800

    first stab at this

--- src/asfLidarViz.c
+++ src/asfLidarViz.c
@@ -9,6 +9,7 @@
 #include <GL/glut.h>    // Header File For The GLUT Library
 #include <GL/gl.h>  // Header File For The OpenGL32 Library
 #include <GL/glu.h> // Header File For The GLu32 Library
+
 #include <unistd.h>     // Header File For sleeping.
 #include <stdio.h>      // Header file for standard file i/o.
 #include <stdlib.h>     // Header file for malloc/free.
@@ -33,6 +34,7 @@ int blend = 0;        // blending on/off
 
 GLfloat xrot;            // x rotation
 GLfloat yrot;            // y rotation
+GLfloat zrot;            // z rotation
 GLfloat xspeed;          // x rotation speed
 GLfloat yspeed;          // y rotation speed
 
@@ -51,6 +53,49 @@ GLfloat LightDiffuse[]  = {1.0f, 1.0f, 1.0f, 1.0f};
 GLfloat LightPosition[] = {0.0f, 0.0f, 2.0f, 1.0f};
 
 GLuint filter = 0;       // texture filtering method to use (nearest, linear, linear + mipmaps)
+
+//fps stuff
+void *font = GLUT_BITMAP_9_BY_15;
+int bitmapHeight=13;
+int frame,time,timebase=0;
+char s[30];
+void renderBitmapString(float x, float y, char *string)
+{
+    char *c;
+    glRasterPos2f(x, y);
+    for (c=string; *c != '\0'; c++) {
+        glutBitmapCharacter(font, *c);
+    }
+}
+void setOrthographicProjection() {
+    int w = 640;
+    int h = 480;
+
+    // switch to projection mode
+    glMatrixMode(GL_PROJECTION);
+    // save previous matrix which contains the
+    //settings for the perspective projection
+    glPushMatrix();
+    // reset matrix
+    glLoadIdentity();
+    // set a 2D orthographic projection
+    gluOrtho2D(0, w, 0, h);
+    // invert the y axis, down is positive
+    glScalef(1, -1, 1);
+    // mover the origin from the bottom left corner
+    // to the upper left corner
+    glTranslatef(0, -h, 0);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void resetPerspectiveProjection() {
+    // set the current matrix to GL_PROJECTION
+    glMatrixMode(GL_PROJECTION);
+    // restore previous settings
+    glPopMatrix();
+    // get back to GL_MODELVIEW matrix
+    glMatrixMode(GL_MODELVIEW);
+}
 
 // degrees to radians...2 PI radians = 360 degrees
 float rad(float angle)
@@ -80,6 +125,9 @@ void readstr(FILE *f, char *string)
     } while ((string[0] == '/') || (string[0] == '\n'));
     return;
 }
+
+
+
 
 // loads the world from a text file.
 void SetupWorld()
@@ -143,6 +191,7 @@ void InitGL(int Width, int Height)          // We call this right after our Open
 
     glMatrixMode(GL_MODELVIEW);
 
+
     // set up lights.
     glLightfv(GL_LIGHT1, GL_AMBIENT, LightAmbient);
     glLightfv(GL_LIGHT1, GL_DIFFUSE, LightDiffuse);
@@ -172,14 +221,38 @@ void ReSizeGLScene(int Width, int Height)
 
 }
 
+void DrawGLPlane()
+{
+    // Drawing The Coordinate Plane Starts Here.
+    // We Will Draw Horizontal And Vertical Lines With A Space Of 1 Meter Between Them.
 
+    glColor3ub(64, 80, 64);                                      // Draw In gray
+    glBegin(GL_LINES);
+
+    // Draw The Vertical Lines
+    for (float x = -20; x <= 20; x += 1.0f)                     // x += 1.0f Stands For 1 Meter Of Space In This Example
+    {
+        glVertex3f(x, 20, 0);
+        glVertex3f(x,-20, 0);
+    }
+
+    // Draw The Horizontal Lines
+    for (float y = -20; y <= 20; y += 1.0f)                     // y += 1.0f Stands For 1 Meter Of Space In This Example
+    {
+        glVertex3f( 20, y, 0);
+        glVertex3f(-20, y, 0);
+    }
+
+    glEnd();
+    // Drawing The Coordinate Plane Ends Here.
+}
 
 /* The main drawing function. */
 void DrawGLScene()
 {
     GLfloat xm, ym, zm;
     GLfloat xtrans, ztrans, ytrans;
-    GLfloat sceneroty;
+    GLfloat sceneroty, scenerotz;
     int numpoints;
 
     // calculate translations and rotations.
@@ -188,6 +261,7 @@ void DrawGLScene()
     ytrans = 0;
 
     sceneroty = 360.0f - yrot;
+    scenerotz = 360.0f - zrot;
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);       // Clear The Screen And The Depth Buffer
     glLoadIdentity();             // Reset The View
@@ -198,13 +272,17 @@ void DrawGLScene()
         glutReshapeWindow(640, 480);
 
     glRotatef(lookupdown, 1.0f, 0, 0);
-    glRotatef(sceneroty, 0, 1.0f, 0);
+    //glRotatef(sceneroty, 0, 1.0f, 0);
+    glRotatef(scenerotz, 0, 0, 1.0f);
 
     glTranslatef(xtrans, ytrans, ztrans);
 
     // Position Camera 40 Meters Up In Z-Direction.
     // Set The Up Vector In Y-Direction So That +X Directs To Right And +Y Directs To Up On The Window.
-    gluLookAt(0, 0, 40, 0, 0, 0, 0, 1, 0);
+    gluLookAt(0, -20, 10, 0, 0, 0, 0, 1, 0);
+
+    // Draw the coordinate plane
+    DrawGLPlane();
 
     numpoints = sector1.numpoints;
 
@@ -219,6 +297,23 @@ void DrawGLScene()
             glVertex3f( xm, ym, zm);
         glEnd();
     }
+
+    // display fps
+    frame++;
+    time=glutGet(GLUT_ELAPSED_TIME);
+    if (time - timebase > 1000) {
+        sprintf(s,"FPS:%4.2f",frame*1000.0/(time-timebase));
+        timebase = time;
+        frame = 0;
+    }
+    glColor3f(0.0f,1.0f,1.0f);
+    setOrthographicProjection();
+    glPushMatrix();
+    glLoadIdentity();
+    renderBitmapString(30,35,s);
+    renderBitmapString(30,55,"Esc - Quit");
+    glPopMatrix();
+    resetPerspectiveProjection();
 
     // swap buffers to display, since we're double buffered.
     glutSwapBuffers();
@@ -306,11 +401,11 @@ void specialKeyPressed(int key, int x, int y)
         break;
 
     case GLUT_KEY_LEFT: // look left
-        yrot += 1.5f;
+        zrot += 1.5f;
         break;
 
     case GLUT_KEY_RIGHT: // look right
-        yrot -= 1.5f;
+        zrot -= 1.5f;
         break;
 
     default:
